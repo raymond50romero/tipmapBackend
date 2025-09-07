10ae129423684db5eecd0b36c687f4650e8ced29
Raymond Romero <raymond50romero@gmail.com>
2025-09-07 14:54:17 -0700

    switched sequelize connect to mariadb

--- database/models/connect.js
+++ database/models/connect.js
@@ -1,18 +1,36 @@
-import 'dotenv/config';
-import { Sequelize } from 'sequelize';
+import "dotenv/config";
+import { Sequelize } from "sequelize";
 
+const databaseName = process.env.DATABASE_NAME;
+const databaseUsername = process.env.DATABASE_USERNAME;
+const databaseHost = process.env.DATABASE_HOST;
+const databasePassword = process.env.DATABASE_PASSWORD;
+
+/*
+ *
+ * mysql connect
+ *
 export const sequelize = new Sequelize(
   process.env.DATABASE_NAME,
   process.env.DATABASE_USERNAME,
   process.env.DATABASE_PASSWORD,
-  { host: process.env.DATABASE_HOST, dialect: 'mysql' }
+  { host: process.env.DATABASE_HOST, dialect: "mysql" },
 );
+*/
+
+export const sequelize = new Sequelize({
+  dialect: "mariadb",
+  database: databaseName,
+  user: databaseUsername,
+  password: databasePassword,
+  host: databaseHost,
+});
 
 sequelize
   .authenticate()
   .then(() => {
-    console.log('it synced');
+    console.log("it synced");
   })
   .catch((error) => {
-    console.log('unable to connect', error);
+    console.log("unable to connect", error);
   });
