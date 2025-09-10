c211b3ea61fc72fd700bf9e83e27b48d921a2fd5
Raymond Romero <raymond50romero@gmail.com>
2025-09-09 23:49:56 -0700

    working on new post bugs

--- routes/newPost/newPost.js
+++ routes/newPost/newPost.js
@@ -20,27 +20,38 @@ router.post("/", authorizeUser, async (req, res) => {
 
   const user = req.user;
 
-  if (!name) res.status(400).send("Restaurant name missing");
-  if (!address) res.status(400).send("Restaurant address missing");
-  if (!weekdayTips) res.status(400).send("Average weekday tips missing");
-  if (!weekendTips) res.status(400).send("Average weekend tips missing");
-  if (!weekendTips) res.status(400).send("Average weekend tips missing");
-  if (!workenv) res.status(400).send("Work environment rating missing");
-  if (!management) res.status(400).send("Management rating missing");
-  if (!clientele) res.status(400).send("Clientele rating missing");
+  if (!name) return res.status(400).send("Restaurant name missing");
+  if (!address) return res.status(400).send("Restaurant address missing");
+  if (!weekdayTips) return res.status(400).send("Average weekday tips missing");
+  if (!weekendTips) return res.status(400).send("Average weekend tips missing");
+  if (!weekendTips) return res.status(400).send("Average weekend tips missing");
+  if (!workenv) return res.status(400).send("Work environment rating missing");
+  if (!management) return res.status(400).send("Management rating missing");
+  if (!clientele) return res.status(400).send("Clientele rating missing");
 
-  const newPost = await createNewPost(
-    user.user_id,
-    name,
-    address,
-    weekdayTips,
-    weekendTips,
-    workenv,
-    management,
-    clientele,
-    title,
-    comment,
-  );
+  try {
+    const newPost = await createNewPost(
+      user.user_id,
+      name,
+      address,
+      weekdayTips,
+      weekendTips,
+      workenv,
+      management,
+      clientele,
+      title,
+      comment,
+    );
+
+    if (newPost) {
+      return res.status(201).send("New post created");
+    } else {
+      return res.status(500).send("Server error, could not create new post");
+    }
+  } catch (error) {
+    console.log("could not make new post \n", error);
+    return res.status(500).send("Server error, could not create new post");
+  }
 });
 
 export default router;
