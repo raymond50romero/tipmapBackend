c7126f054d8ec902a7de795dccbe3964cd64a8db
Raymond Romero <raymond50romero@gmail.com>
2025-10-02 00:32:57 -0700

    working on weights

---
+++ routes/posts/utils/weights.js
@@ -0,0 +1,15 @@
+export function getWeights(posts) {
+  if (!posts) return false;
+
+  // first find the mean rbar
+  let r_bar = 0;
+  for (let i = 0; i < posts.length; i++) {}
+}
+
+export function getGlobalMean(posts) {
+  if (!posts) return false;
+
+  const muArray = [];
+
+  for (let i = 0; i < posts.length; i++) {}
+}
